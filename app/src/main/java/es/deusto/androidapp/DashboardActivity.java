1d42ccf15cf6f39936d1027471cfcf8b7274590c
Ander Arguiñano <ander.arguinano@opendeusto.es>
2020-05-05 12:30:30 +0200

    Started creating home layout

--- app/src/main/java/es/deusto/androidapp/DashboardActivity.java
+++ app/src/main/java/es/deusto/androidapp/DashboardActivity.java
@@ -3,6 +3,8 @@ package es.deusto.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+
+    public void addRecipe(View view) {
+        Log.i("RECIPE", "Starting creating recipe");
+
     }
 }
