85a64723526542a76172c71d2dde4418f18477dc
Ander Arguiñano <ander.arguinano@opendeusto.es>
2020-05-07 13:27:00 +0200

    Starting with the user layout

--- app/src/main/java/es/deusto/androidapp/UserFragment.java
+++ app/src/main/java/es/deusto/androidapp/UserFragment.java
@@ -2,14 +2,20 @@ package es.deusto.androidapp;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class UserFragment extends Fragment {
+
+    private AppCompatButton updateButton;
+    private AppCompatButton deleteButton;
+
 
     public UserFragment() {
         // Required empty public constructor
@@ -32,7 +38,39 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_user,
+                container, false);
+
+        updateButton = view.findViewById(R.id.update_button);
+
+        updateButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                updateUser();
+            }
+        });
+
+        deleteButton = view.findViewById(R.id.delete_button);
+
+        deleteButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                deleteUser();
+            }
+        });
+        return view;
+    }
+
+    public void updateUser() {
+        Log.i("RECIPE", "Updating User");
+    }
+
+    public void deleteUser() {
+        Log.i("RECIPE", "Deleting User");
     }
 }
