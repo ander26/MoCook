2ad4abcdf34393ff04fb228e9db2ececbd315897
Ander Arguiñano <ander.arguinano@opendeusto.es>
2020-05-07 10:27:26 +0200

    Starting with the user profile activity

---
+++ app/src/main/java/es/deusto/androidapp/UserProfileAcitivty.java
@@ -0,0 +1,14 @@
+package es.deusto.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_acitivty);
+    }
+}
