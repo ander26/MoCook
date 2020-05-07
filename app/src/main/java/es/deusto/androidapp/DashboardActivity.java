de00c9ba34b54298a6de73593e4e5b5aa0453d7d
Ander Arguiñano <ander.arguinano@opendeusto.es>
2020-05-07 12:02:49 +0200

    Created user and home activities as fragments

--- app/src/main/java/es/deusto/androidapp/DashboardActivity.java
+++ app/src/main/java/es/deusto/androidapp/DashboardActivity.java
@@ -1,11 +1,17 @@
 package es.deusto.androidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -13,12 +19,46 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        fragment = HomeFragment.newInstance();
+                        break;
+                    case R.id.navigation_liked:
+                        break;
+                    case R.id.navigation_user:
+                        fragment = UserFragment.newInstance();
+                        break;
+                }
+                replaceFragment(fragment);
+                return true;
+            }
+        });
+
+        setInitialFragment();
+    }
+
+    private void setInitialFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_placeholder, HomeFragment.newInstance());
+        fragmentTransaction.commit();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_placeholder, fragment);
+        fragmentTransaction.commit();
     }
 
 
     public void addRecipe(View view) {
        Intent intent = new Intent(this, CreateRecipeActivity.class);
        startActivity(intent);
-
     }
 }
---
+++ app/src/main/java/es/deusto/androidapp/UserFragment.java
@@ -0,0 +1,38 @@
+package es.deusto.androidapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UserFragment extends Fragment {
+
+    public UserFragment() {
+        // Required empty public constructor
+    }
+
+    public static UserFragment newInstance() {
+        UserFragment fragment = new UserFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+}
